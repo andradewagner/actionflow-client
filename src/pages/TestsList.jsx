7a788a117fa59dd0d3ced1ce39bd15c38c7dd29b
Wagner Andrade <andradewagner@hotmail.com>
2020-03-12 17:02:44 -0300

    react-table

--- src/pages/TestsList.jsx
+++ src/pages/TestsList.jsx
@@ -1,11 +1,144 @@
 import React, { Component } from 'react'
+import ReactTable from 'react-table'
+import api from '../api'
+
+import styled from 'styled-components'
+
+//import 'react-table/react-table.css'
+
+const Wrapper = styled.div`
+    padding: 0 40px 40px 40px;
+`
+
+const Update = styled.div`
+    color: #ef9b0f;
+    cursor: pointer;
+`
+
+const Delete = styled.div`
+    color: #ff0000;
+    cursor: pointer;
+`
+
+class UpdateTest extends Component {
+    updateUser = event => {
+        event.preventDefault()
+
+        window.location.href = `/tests/update/${this.props.id}`
+    }
+
+    render() {
+        return <Update onClick={this.updateUser}>Update</Update>
+    }
+}
+
+class DeleteTest extends Component {
+    deleteUser = event => {
+        event.preventDefault()
+
+        if (
+            window.confirm(
+                `Do tou want to delete the test ${this.props.id} permanently?`,
+            )
+        ) {
+            api.deleteTestById(this.props.id)
+            window.location.reload()
+        }
+    }
+
+    render() {
+        return <Delete onClick={this.deleteUser}>Delete</Delete>
+    }
+}
 
 class TestsList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            tests: [],
+            columns: [],
+            isLoading: false,
+        }
+    }
+
+    componentDidMount = async () => {
+        this.setState({ isLoading: true })
+
+        await api.getAllTests().then(tests => {
+            this.setState({
+                tests: tests.data.data,
+                isLoading: false,
+            })
+        })
+    }
+
     render() {
+        const { tests, isLoading } = this.state
+        console.log('TCL: TestsList -> render -> tests', tests)
+
+        const columns = [
+            {
+                Header: 'ID',
+                accessor: '_id',
+                filterable: true,
+            },
+            {
+                Header: 'Name',
+                accessor: 'name',
+                filterable: true,
+            },
+            {
+                Header: 'Rating',
+                accessor: 'rating',
+                filterable: true,
+            },
+            {
+                Header: 'Time',
+                accessor: 'time',
+                Cell: props => <span>{props.value.join(' / ')}</span>,
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <DeleteTest id={props.original._id} />
+                        </span>
+                    )
+                },
+            },
+            {
+                Header: '',
+                accessor: '',
+                Cell: function(props) {
+                    return (
+                        <span>
+                            <UpdateTest id={props.original._id} />
+                        </span>
+                    )
+                },
+            },
+        ]
+
+        let showTable = true
+        if (!tests.length) {
+            showTable = false
+        }
+
         return (
-            <div>
-                <p>In this page you'll see the table with the tests</p>
-            </div>
+            <Wrapper>
+                {showTable && (
+                    <ReactTable
+                        data={tests}
+                        columns={columns}
+                        loading={isLoading}
+                        defaultPageSize={10}
+                        showPageSizeOptions={true}
+                        minRows={0}
+                    />
+                )}
+            </Wrapper>
         )
     }
 }
